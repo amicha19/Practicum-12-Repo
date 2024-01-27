d93ebf4bd5477769ba77f144c6fe37f080b43d7f
Chuck476 <cdierbach@towson.edu>
2024-01-26 19:31:46 -0500

    Create main.java

---
+++ main.java
@@ -0,0 +1,23 @@
+// Practicum 12 (Yes/No Program)
+// (TO WRITE)
+
+// This program should prompt the user to enter "yes" or
+// "no" and output their response. The user should be allowed to
+// enter their response with either upper or lower case characters
+// (e.g., "yes", "Yes", "YES").
+
+// REQUIRED OUTPUT:
+// The program MUST display the results exactly as shown below:
+// "User entered yes"
+// "User entered no"
+//
+// (TO COMPLETE)
+public class Main {
+    public static void main(String[] args) {
+
+      System.out.println("Hello World!");
+
+
+    }
+
+}
